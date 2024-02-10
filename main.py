0ba9cfdc5e0b85e3b04fc029bbcb7f317bfdd510
Влад Ефремов <vladefr97@yandex.ru>
2024-02-10 10:17:18 +0300

    main.py modified

--- main.py
+++ main.py
@@ -13,4 +13,8 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def some_func():
+    return "some string"
