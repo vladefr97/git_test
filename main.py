7d0e9539dd03c8e2cc3a01b3e111b108f8fad885
Влад Ефремов <vladefr97@yandex.ru>
2024-02-10 10:38:19 +0300

    some changes

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def print_hi(name):
 def some_func():
     return "branch main new"
 
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print(some_func())
